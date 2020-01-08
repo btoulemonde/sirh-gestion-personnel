a66f2a05e611db369e70fc1c3dce0aa08a5817f0
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-01-08 15:05:22 +0100

    tp02 fin

--- src/main/java/dev/sgp/web/EditerCollaborateursController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateursController.java
@@ -20,27 +20,32 @@ public class EditerCollaborateursController extends HttpServlet {
 		String nom = req.getParameter("nom");
 		String prenom = req.getParameter("prenom");
 
-		List<String> parametres = new ArrayList<>();
+		Map<String,String> parametres = new HashMap<String,String>();
 
-		parametres.add(matricule);
-		parametres.add(titre);
-		parametres.add(nom);
-		parametres.add(prenom);
+		parametres.put("matricule",matricule);
+		parametres.put("titre",titre);
+		parametres.put("nom",nom);
+		parametres.put("prenom",prenom);
 
-	
-			
-			if (matricule == null || "".equals(matricule)) {
+		boolean parmExist = true;
+		List<String> paramMissed = new ArrayList<>();
+			for(String param:parametres.keySet()){
+				if (parametres.get(param) == null || "".equals(parametres.get(param))) {
 				resp.setContentType("text/html");
 				resp.setStatus(400);
-				resp.getWriter().write("<p>un matricule etait attendu</p>");
-
-			} else {
+				parmExist = false;
+				paramMissed.add(param);
+				} 
+			}
+			
+			if (parmExist == false){
+				resp.getWriter().write("<p>Les paramètres suivants sont incorrects :</p>"+paramMissed.toString());
+			}else {
 				resp.setStatus(201);
 				resp.getWriter()
 						.write("<h1>Edition de collaborateur</h1>" + "<ul><li>matricule : " + matricule
 								+ "</li><li>titre : " + titre + "</li><li>nom : " + nom + "</li><li>prenom : " + prenom
 								+ "</li></ul>");
 			}
-		
 	}
 }
